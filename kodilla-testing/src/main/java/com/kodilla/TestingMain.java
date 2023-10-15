d0fdae56bd56f02858be19e6ac0007aea780bfec
Marcin Praski <praski.martin@gmail.com>
2023-10-15 11:42:26 +0200

    6.2 Added kodilla-testing calculator

--- kodilla-testing/src/main/java/com/kodilla/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/TestingMain.java
@@ -1,6 +1,7 @@
 package com.kodilla;
 
 import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
 
@@ -11,6 +12,23 @@ public class TestingMain {
 
         if (result.equals("theForumUser")) {
             System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int addAToB = calculator.add(25,10);
+        int subtractAFromB = calculator.subtract(25,10);
+
+        if (addAToB == 35){
+            System.out.println("test addAToB OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (subtractAFromB == 15){
+            System.out.println("test subtractAFromB OK");
         } else {
             System.out.println("Error!");
         }
