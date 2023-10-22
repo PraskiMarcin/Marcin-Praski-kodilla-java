0d89fbd009d20294f8779c045ee6bceec9476b45
Marcin Praski <praski.martin@gmail.com>
2023-10-22 18:35:30 +0200

    6.4 shape collector - implementation

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -5,17 +5,35 @@ import java.util.List;
 
 public class ShapeCollector {
 
-    private List<Shape> shapeCollector = new ArrayList<>();
+    private List<Shape> elements = new ArrayList<>();
 
 
     public void addFigure(Shape shape){
+        elements.add(shape);
     }
     public void removeFigure(Shape shape){
+       elements.remove(shape);
     }
     public Shape getFigure(int n){
-        return null;
+        if (n >= elements.size()){
+            return null;
+        }
+        return elements.get(n);
     }
     public String showFigures(){
-        return null;
+        if (elements.isEmpty()) {
+            return  "";
+        }
+
+        String result = "";
+        boolean addSeparator = false;
+        for(Shape shape:elements){
+            if (addSeparator){
+                result +=",";
+            }
+            result += shape.getShapeName();
+            addSeparator = true;
+        }
+        return result;
     }
 }
